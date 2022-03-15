54b22afdec8770038af1b45920ff17e60a2db8f4
Mahmudul <bappy392@gmail.com>
2022-03-15 21:10:06 +0600

    Register component

---
+++ src/Register.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import axios from 'axios'
+
+const Register=()=>{
+
+  const [email, setEmail]=useState('')
+  const [password, setPassword]=useState('')
+
+   const registerUser=(e)=>{
+      e.preventDefault()
+      
+   }
+
+    return(
+       <form action="" onSubmit={e=>registerUser(e)}>
+            <h2>Register</h2>
+            
+            <input type='email' placeholder="Enter Your Email" required onChange={event=>setEmail(event.target.value)} value={email}/>
+            <input type='password' placeholder="Enter Your Password" required onChange={event=>setPassword(event.target.value)} value={password}/>
+
+            <button type="submit">Register</button>
+       </form> 
+ 
+    )
+ 
+ }
+ 
+ export default Register
