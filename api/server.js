e865e09129de9deb3d5bcc35d6d44e2146493703
Mahmudul <bappy392@gmail.com>
2022-03-17 14:52:25 +0600

    17.3.22(2)

--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+import cookieParser  from 'cookie-parser'
 import mongoose from 'mongoose'
 import User from './models/User.js';
 import bcrypt from 'bcrypt'
 // import cors from 'cors'
+
+const app = express()
+app.use(bodyParser.json({extended:true}))
+app.use(cookieParser())
+// app.use(cors({
+//   credentials:true,
+//   origin:'http://localhost:3000'
+// }))
 
 //mongodb://127.0.0.1:27017/
 await mongoose.connect('mongodb://127.0.0.1:27017/auth', {useNewUrlParser:true, useUnifiedTopology:true})
